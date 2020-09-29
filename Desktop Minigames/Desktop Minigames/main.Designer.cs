7446d9e49eac0e21d8d45a026e7d5be0cb67674d
74805 <yuvtaub1308@gmail.com>
2020-09-29 17:42:29 +0300

    main
    main

--- Desktop Minigames/Desktop Minigames/main.Designer.cs
+++ Desktop Minigames/Desktop Minigames/main.Designer.cs
@@ -37,6 +37,7 @@
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Name = "Minigames";
             this.Text = "Minigames";
+            this.Load += new System.EventHandler(this.Minigames_Load);
             this.ResumeLayout(false);
 
         }
---
+++ Desktop Minigames/Desktop Minigames/main.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Desktop_Minigames
+{
+    public partial class Minigames : Form
+    {
+        public Minigames()
+        {
+            Width = (int)(Screen.PrimaryScreen.WorkingArea.Size.Width / 3.5);
+            Height = (int)(Screen.PrimaryScreen.WorkingArea.Size.Height / 1.25);
+
+            Label minigames = new Label();
+            minigames.Text = "Minigames";
+            minigames.Font = new Font("Ariel", 30);
+            minigames.Size = new Size(Width/2 , Height/9);
+            minigames.Location = new Point(Width / 2 -(int)(minigames.Size.Width /2.4), Height / 10);
+            Controls.Add(minigames);
+
+            Button snake = new Button();
+            snake.Text = "Snake";
+            snake.Font = new Font("Ariel", 25);
+            snake.Size = new Size(150,150);
+            snake.Location = new Point(Width / 7, Height / 4);
+            snake.Click += Snake;
+            Controls.Add(snake);
+
+            Button solitaire = new Button();
+            solitaire.Text = "Solitaire";
+            solitaire.Font = new Font("Ariel", 25);
+            solitaire.Size = new Size(150, 150);
+            solitaire.Location = new Point((int)(Width / 1.8), Height / 4);
+            solitaire.Click += Solitaire;
+            Controls.Add(solitaire);
+
+            Button flappy = new Button();
+            flappy.Text = "Flappy Bird";
+            flappy.Font = new Font("Ariel", 25);
+            flappy.Size = new Size(150, 150);
+            flappy.Location = new Point(Width / 7, (int)(Height / 2.15));
+            flappy.Click += Flappy;
+            Controls.Add(flappy);
+
+            Button whist = new Button();
+            whist.Text = "Whist";
+            whist.Font = new Font("Ariel", 25);
+            whist.Size = new Size(150, 150);
+            whist.Location = new Point((int)(Width / 1.8), (int)(Height / 2.15));
+            whist.Click += Flappy;
+            Controls.Add(whist);
+        }
+        public static void Snake(object sender,EventArgs args)
+        {
+
+        }
+
+        public static void Whist(object sender, EventArgs args)
+        {
+
+        }
+
+        public static void Flappy(object sender, EventArgs args)
+        {
+
+        }
+
+        public static void Solitaire(object sender, EventArgs args)
+        {
+
+        }
+        private void Minigames_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
