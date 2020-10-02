e92494a28089997b93fe58998b5019f741e5ec81
Ofirfr <56976381+Ofirfr@users.noreply.github.com>
2020-10-02 15:59:20 +0300

    Added Flappy bird - not final

---
+++ Desktop Minigames/Desktop Minigames/Consts.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace flappy_bird
+{
+    class Consts
+    {
+        public const int BIRD_HEIGHT = 50;
+        public const int BIRD_WIDTH = 30;
+        public const int PIPE_WIDTH = 75;
+        public const int BIRD_X = 200;                         
+
+        public static Bitmap ResizeImage(Image image, int width, int height)
+        {
+            var destRect = new Rectangle(0, 0, width, height);
+            var destImage = new Bitmap(width, height);
+
+            destImage.SetResolution(image.HorizontalResolution, image.VerticalResolution);
+
+            using (var graphics = Graphics.FromImage(destImage))
+            {
+                graphics.CompositingMode = CompositingMode.SourceCopy;
+                graphics.CompositingQuality = CompositingQuality.HighQuality;
+                graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                graphics.SmoothingMode = SmoothingMode.HighQuality;
+                graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
+
+                using (var wrapMode = new ImageAttributes())
+                {
+                    wrapMode.SetWrapMode(WrapMode.TileFlipXY);
+                    graphics.DrawImage(image, destRect, 0, 0, image.Width, image.Height, GraphicsUnit.Pixel, wrapMode);
+                }
+            }
+
+            return destImage;
+        }
+    }
+}
---
+++ Desktop Minigames/Desktop Minigames/Pipe.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace flappy_bird
+{
+    public class Pipe
+    {
+        private Label lowerPipe;
+        private Label higherPipe;
+        public Label LowerPipe { get => lowerPipe; set => lowerPipe = value; }
+        public Label HigherPipe { get => higherPipe; set => higherPipe = value; }
+    }
+}
