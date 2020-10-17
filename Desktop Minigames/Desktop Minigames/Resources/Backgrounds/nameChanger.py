17d042fbcfa58521e00299221177f12eb35f1738
LiorBitton <liorb5000@gmail.com>
2020-10-17 19:17:58 +0300

    Update nameChanger.py

--- Desktop Minigames/Desktop Minigames/Resources/Backgrounds/nameChanger.py
+++ Desktop Minigames/Desktop Minigames/Resources/Backgrounds/nameChanger.py
@@ -1,6 +1,13 @@
 import os
+import cv2 as cv
 files = os.listdir()
+photo = ['.png', '.jpg', '.jpeg']
 for file in files:
-    if ".py" in file:
-       os.rename(file, './background' + file)
-print(os.listdir())
+    if ".py" not in file:
+        """
+        frame = cv.imread(file, cv.IMREAD_GRAYSCALE)
+        height = len(frame)
+        width = len(frame[0])
+        print(f"{height} + {width}")"""
+        if not file.startswith('background'):
+            os.rename(file, './background' + file)
